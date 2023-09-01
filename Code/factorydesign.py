c1b2e1a2e57f3502115942ab17c9eb6a3ff496ab
Nathan Konigkramer <98358837+NateKonigkramer214@users.noreply.github.com>
2023-09-01 13:22:32 +1200

    Code

--- Code/factorydesign.py
+++ Code/factorydesign.py
@@ -1,3 +1,4 @@
+# Import statements (unchanged)
 import pandas as pd 
 import seaborn as sns 
 import matplotlib.pyplot as plt
@@ -10,6 +11,21 @@ from sklearn.ensemble import GradientBoostingRegressor
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error
 from joblib import dump, load
+
+# Factory function to create models
+def create_model(model_name):
+    if model_name == 'Linear Regression':
+        return LinearRegression()
+    elif model_name == 'Support Vector Machine':
+        return SVR()
+    elif model_name == 'Random Forest':
+        return RandomForestRegressor()
+    elif model_name == 'Gradient Boosting Regressor':
+        return GradientBoostingRegressor()
+    elif model_name == 'XGBRegressor':
+        return XGBRegressor()
+    else:
+        raise ValueError("Invalid model name")
 
 # Load the dataset
 def load_data(filepath):
@@ -87,22 +103,32 @@ def predict_new_data(input_data, scaler, loaded_model, scaler1):
     original_pred_value = scaler1.inverse_transform(pred_value)
     return original_pred_value
 
-# Main function
+
+# Main function with factory pattern
 def main():
     filepath = r'D:\Machine_Learning_Python\Car_Purchasing_Data.xlsx'
     data = load_data(filepath)
     X_scaled, Y_scaled, scaler, scaler1 = preprocess_data(data)
     X_train, X_test, y_train, y_test = split_data(X_scaled, Y_scaled)
-    trained_models = train_models(X_train, y_train)
+    
+    model_names = ['Linear Regression', 'Support Vector Machine', 'Random Forest', 'Gradient Boosting Regressor', 'XGBRegressor']
+    
+    trained_models = {}
+    for name in model_names:
+        model = create_model(name)
+        model.fit(X_train, y_train)
+        trained_models[name] = model
+    
     rmse_values = evaluate_models(trained_models, X_test, y_test, scaler1)
     plot_model_performance(rmse_values)
     save_best_model(trained_models, rmse_values)
     
     # Example input for prediction
     input_data = [0, 30, 60000, 2000, 40000]  # Replace with actual input
-    loaded_model = load("best_model.joblib")
+    model_name = min(rmse_values, key=rmse_values.get)
+    loaded_model = trained_models[model_name]
     prediction = predict_new_data(input_data, scaler, loaded_model, scaler1)
-    print("Predicted Car Purchase Amount:", prediction)
+    print(f"Predicted Car Purchase Amount using {model_name}:", prediction)
 
 if __name__ == "__main__":
     main()
